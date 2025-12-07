9c5e7d65ca0b7187eaa4f576d53ecd0075ec7ac0
Dipesh Kumar <dipeshkumar0853822@gmail.com>
2025-12-07 12:04:56 +0530

    fixed the exposure of the database_url

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, jsonify
 from flask import jsonify
-from database import jobs
+from database import load_jobs
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", jobs = jobs)
+    return render_template("index.html", jobs = load_jobs())
 @app.route("/api/jobs")
 def jobsapi():
     return jsonify(jobs)
--- database.py
+++ database.py
@@ -1,12 +1,19 @@
-
 from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
 
-DB_URL = "mysql+pymysql://sql12810877:CjUfN1Tclg@sql12.freesqldatabase.com:3306/sql12810877"
+load_dotenv()
 
-engine = create_engine(DB_URL)
+DATABASE_URL = os.getenv("DB_URL")
 
-jobs = []
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs"))
-    for row in result.all():
-        jobs.append(dict(row._mapping))
+engine = create_engine(DATABASE_URL)
+
+
+def load_jobs():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row._mapping))
+        return jobs
+
